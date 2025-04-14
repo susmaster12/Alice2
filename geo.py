00d4747463da95f013e1d76c67c697afcf3d2429
Ivan_n+2k <ryzptdbdfy@mail.ru>
2025-04-14 19:58:18 +0300

    Расстояние между городами

--- geo.py
+++ geo.py
@@ -8,7 +8,7 @@ def get_coordinates(city):
     params = {
         'geocode': city,
         'format': 'json',
-        'apikey': "40d1649f-0493-4b70-98ba-98533de7710b"
+        'apikey': "952a7df1-f366-4423-a097-f05cb4a71db2"
     }
 
     response = requests.get(url, params)
@@ -25,7 +25,7 @@ def get_country(city):
     params = {
         'geocode': city,
         'format': 'json',
-        'apikey': "40d1649f-0493-4b70-98ba-98533de7710b"
+        'apikey': "952a7df1-f366-4423-a097-f05cb4a71db2"
     }
 
     response = requests.get(url, params)
